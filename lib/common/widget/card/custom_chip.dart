6ee28a09d9b68c82bfdaadfe43444131df0b4497
sumitkakshapati <me.summet@gmail.com>
2022-09-24 14:54:57 +0545

    custom chip added

---
+++ lib/common/widget/card/custom_chip.dart
@@ -0,0 +1,62 @@
+import 'package:boilerplate/app/theme.dart';
+import 'package:flutter/material.dart';
+
+class CustomChip extends StatelessWidget {
+  final String label;
+  final Color backgroundColor;
+  final double verticalPadding;
+  final double horizontalPadding;
+  final double borderRadius;
+  final double fontSize;
+  final Color textColor;
+  final double leftmargin;
+  final double rightMargin;
+  final VoidCallback? onPressed;
+  const CustomChip({
+    Key? key,
+    required this.label,
+    this.backgroundColor = CustomTheme.primaryColor,
+    this.horizontalPadding = 16,
+    this.verticalPadding = 12,
+    this.borderRadius = 8,
+    this.fontSize = 14,
+    this.leftmargin = 0,
+    this.rightMargin = 0,
+    this.textColor = Colors.white,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
+    final _textTheme = _theme.textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(
+        left: leftmargin,
+        right: rightMargin,
+      ),
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          child: Text(
+            label,
+            style: _textTheme.headline6!.copyWith(
+              fontWeight: FontWeight.w400,
+              color: textColor,
+              fontSize: fontSize,
+            ),
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: horizontalPadding,
+            vertical: verticalPadding,
+          ),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/shipping/ui/widgets/shipping_widgets.dart
+++ lib/feature/shipping/ui/widgets/shipping_widgets.dart
@@ -1,13 +1,28 @@
 import 'package:boilerplate/app/theme.dart';
 import 'package:boilerplate/common/util/size_utils.dart';
 import 'package:boilerplate/common/widget/button/custom_icon_button.dart';
+import 'package:boilerplate/common/widget/card/custom_chip.dart';
 import 'package:boilerplate/common/widget/text_field/search_textfield.dart';
 import 'package:boilerplate/feature/shipping/ui/widgets/shipment_card.dart';
 import 'package:flutter/material.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
-class ShippingWidgets extends StatelessWidget {
+class ShippingWidgets extends StatefulWidget {
   const ShippingWidgets({Key? key}) : super(key: key);
+
+  @override
+  State<ShippingWidgets> createState() => _ShippingWidgetsState();
+}
+
+class _ShippingWidgetsState extends State<ShippingWidgets> {
+  final List<String> _shipmentStatus = [
+    "All Shipping",
+    "On Transit",
+    "Received",
+    "Returned",
+  ];
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +36,62 @@ class ShippingWidgets extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.only(
                 top: MediaQuery.of(context).viewPadding.top + 12,
-                left: CustomTheme.symmetricHozPadding,
-                right: CustomTheme.symmetricHozPadding,
                 bottom: 20.hp,
               ),
               color: _theme.scaffoldBackgroundColor,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Column(
                 children: [
-                  const Expanded(
-                    child: SearchTextField(
-                      hintText: "Search By Shipment ID",
-                      bottomPadding: 0,
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: CustomTheme.symmetricHozPadding,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Expanded(
+                          child: SearchTextField(
+                            hintText: "Search By Shipment ID",
+                            bottomPadding: 0,
+                          ),
+                        ),
+                        SizedBox(width: 12.wp),
+                        Center(
+                          child: CustomIconButton(
+                            icon: Icons.photo_filter_rounded,
+                            backgroundColor: _theme.primaryColor,
+                            iconColor: Colors.white,
+                            borderRadius: 12,
+                            padding: 14,
+                            onPressed: () {},
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  SizedBox(width: 12.wp),
-                  Center(
-                    child: CustomIconButton(
-                      icon: Icons.photo_filter_rounded,
-                      backgroundColor: _theme.primaryColor,
-                      iconColor: Colors.white,
-                      borderRadius: 12,
-                      padding: 14,
-                      onPressed: () {},
+                  SizedBox(height: 20.hp),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(_shipmentStatus.length, (index) {
+                        return CustomChip(
+                          label: _shipmentStatus[index],
+                          leftmargin: CustomTheme.symmetricHozPadding,
+                          rightMargin: (index == _shipmentStatus.length - 1)
+                              ? CustomTheme.symmetricHozPadding
+                              : 0,
+                          backgroundColor: _currentIndex == index
+                              ? CustomTheme.primaryColor
+                              : Colors.white,
+                          textColor: _currentIndex == index
+                              ? Colors.white
+                              : CustomTheme.lightTextColor,
+                          onPressed: () {
+                            setState(() {
+                              _currentIndex = index;
+                            });
+                          },
+                        );
+                      }),
                     ),
                   )
                 ],
